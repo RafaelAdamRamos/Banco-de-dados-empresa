c2e7a4a32249a0c829c7abb941882218099d2fca
Adam 2 <111005987+RafaelAdamRamos@users.noreply.github.com>
2025-06-23 09:57:46 -0300

    Add files via upload

---
+++ Base de dados empresa/src/main/java/org/example/conexao.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexao {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/cliente_db";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "";
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco: " + e.getMessage());
+            return null;
+        }
+    }
+}
